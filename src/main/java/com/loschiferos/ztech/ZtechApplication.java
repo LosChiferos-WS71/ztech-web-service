ea16e0b83746ab75f82401cf42408a3b81d38145
marzzioChicana <u202022228@upc.edu.pe>
2024-05-18 17:21:41 -0500

    first commit

---
+++ src/main/java/com/loschiferos/ztech/ZtechApplication.java
@@ -0,0 +1,13 @@
+package com.loschiferos.ztech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtechApplication.class, args);
+	}
+
+}
