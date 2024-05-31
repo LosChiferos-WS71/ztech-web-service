7713aad6b9e342734f5f3f139783aea66fe8bd82
marzzioChicana <u202022228@upc.edu.pe>
2024-05-30 23:05:14 -0500

    update plant entity

--- src/main/java/com/loschiferos/ztech/flowerpot/domain/model/entities/Plant.java
+++ src/main/java/com/loschiferos/ztech/flowerpot/domain/model/entities/Plant.java
@@ -20,9 +20,37 @@ public class Plant {
     private String description;
 
     private int maxTemperature;
+
     private int maxSunlight;
+
     private int maxHumidity;
+
     private int minTemperature;
+
     private int minSunlight;
+
     private int minHumidity;
+
+    public Plant(String name, String description, int maxTemperature, int maxSunlight, int maxHumidity, int minTemperature, int minSunlight, int minHumidity) {
+        this();
+        this.name = name;
+        this.description = description;
+        this.maxTemperature = maxTemperature;
+        this.maxSunlight = maxSunlight;
+        this.maxHumidity = maxHumidity;
+        this.minTemperature = minTemperature;
+        this.minSunlight = minSunlight;
+        this.minHumidity = minHumidity;
+    }
+
+    public Plant() {
+        this.name = "";
+        this.description = "";
+        this.maxTemperature = 0;
+        this.maxSunlight = 0;
+        this.maxHumidity = 0;
+        this.minTemperature = 0;
+        this.minSunlight = 0;
+        this.minHumidity = 0;
+    }
 }
