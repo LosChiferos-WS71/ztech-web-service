f5e08c2ea8fa677da26c8ac3140cfd45eb506b25
Jeremy_134 <u202022237@upc.edu.pe>
2024-05-30 23:15:45 -0500

    add FlowerpotRecommend aggregate

---
+++ src/main/java/com/loschiferos/ztech/flowerpot/domain/model/aggregates/FlowerpotRecommend.java
@@ -0,0 +1,23 @@
+package com.loschiferos.ztech.flowerpot.domain.model.aggregates;
+
+import com.loschiferos.ztech.flowerpot.domain.model.entities.Plant;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class FlowerpotRecommend {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @ManyToOne
+    @JoinColumn(name = "flowerpot_id")
+    private Plant flowerpotId;
+
+    private String text;
+}
