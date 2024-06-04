756c802b674192e8e1b2c0487904f5703465c9e5
Jeremy_134 <u202022237@upc.edu.pe>
2024-06-03 22:23:53 -0500

    add PlantOwner and Supplier controller

---
+++ src/main/java/com/loschiferos/ztech/profile/interfaces/rest/PlantOwnerController.java
@@ -0,0 +1,67 @@
+package com.loschiferos.ztech.profile.interfaces.rest;
+
+import com.loschiferos.ztech.profile.domain.model.queries.GetAllPlantOwnersQuery;
+import com.loschiferos.ztech.profile.domain.model.queries.GetPlantOwnerByIdQuery;
+import com.loschiferos.ztech.profile.domain.services.PlantOwnerCommandService;
+import com.loschiferos.ztech.profile.domain.services.PlantOwnerQueryService;
+import com.loschiferos.ztech.profile.interfaces.rest.resources.CreatePlantOwnerResource;
+import com.loschiferos.ztech.profile.interfaces.rest.resources.PlantOwnerResource;
+import com.loschiferos.ztech.profile.interfaces.rest.transform.CreatePlantOwnerCommandFromResourceAssembler;
+import com.loschiferos.ztech.profile.interfaces.rest.transform.PlantOwnerResourceFromEntityAssembler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/plant-owners")
+public class PlantOwnerController {
+    private final PlantOwnerCommandService plantOwnerCommandService;
+    private final PlantOwnerQueryService plantOwnerQueryService;
+
+    @Autowired
+    public PlantOwnerController(PlantOwnerCommandService plantOwnerCommandService, PlantOwnerQueryService plantOwnerQueryService) {
+        this.plantOwnerCommandService = plantOwnerCommandService;
+        this.plantOwnerQueryService = plantOwnerQueryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PlantOwnerResource> createSupplier(@RequestBody CreatePlantOwnerResource resource) {
+        var createPlantOwnerCommand = CreatePlantOwnerCommandFromResourceAssembler.toCommandFromResource(resource);
+        var plantOwnerId = plantOwnerCommandService.handle(createPlantOwnerCommand);
+        if (plantOwnerId == 0L) {
+            return ResponseEntity.badRequest().build();
+        }
+        var getPlantOwnerByIdQuery = new GetPlantOwnerByIdQuery(plantOwnerId);
+        var plantOwner = plantOwnerQueryService.handle(getPlantOwnerByIdQuery);
+
+        if (plantOwner.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var plantOwnerResource = PlantOwnerResourceFromEntityAssembler.toResourceFromEntity(plantOwner.get());
+        return new ResponseEntity<>(plantOwnerResource, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{plantOwnerId}")
+    public ResponseEntity<PlantOwnerResource> getPlantOwnerById(@PathVariable Long plantOwnerId) {
+        var getPlantOwnerByIdQuery = new GetPlantOwnerByIdQuery(plantOwnerId);
+        var plantOwner = plantOwnerQueryService.handle(getPlantOwnerByIdQuery);
+        if (plantOwner.isEmpty()) return ResponseEntity.badRequest().build();
+        var plantOwnerResource = PlantOwnerResourceFromEntityAssembler.toResourceFromEntity(plantOwner.get());
+        return ResponseEntity.ok(plantOwnerResource);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlantOwnerResource>> getAllPlantOwners() {
+        var getAllPlantOwnersQuery = new GetAllPlantOwnersQuery();
+        var plantOwners = plantOwnerQueryService.handle(getAllPlantOwnersQuery);
+        var plantOwnerResources = plantOwners.stream()
+                .map(PlantOwnerResourceFromEntityAssembler::toResourceFromEntity)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(plantOwnerResources);
+    }
+}
---
+++ src/main/java/com/loschiferos/ztech/profile/interfaces/rest/SupplierController.java
@@ -0,0 +1,67 @@
+package com.loschiferos.ztech.profile.interfaces.rest;
+
+import com.loschiferos.ztech.profile.domain.model.queries.GetAllSuppliersQuery;
+import com.loschiferos.ztech.profile.domain.model.queries.GetSupplierByIdQuery;
+import com.loschiferos.ztech.profile.domain.services.SupplierCommandService;
+import com.loschiferos.ztech.profile.domain.services.SupplierQueryService;
+import com.loschiferos.ztech.profile.interfaces.rest.resources.CreateSupplierResource;
+import com.loschiferos.ztech.profile.interfaces.rest.resources.SupplierResource;
+import com.loschiferos.ztech.profile.interfaces.rest.transform.CreateSupplierCommandFromResourceAssembler;
+import com.loschiferos.ztech.profile.interfaces.rest.transform.SupplierResourceFromEntityAssembler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/suppliers")
+public class SupplierController {
+    private final SupplierCommandService supplierCommandService;
+    private final SupplierQueryService supplierQueryService;
+
+    @Autowired
+    public SupplierController(SupplierCommandService supplierCommandService, SupplierQueryService supplierQueryService) {
+        this.supplierCommandService = supplierCommandService;
+        this.supplierQueryService = supplierQueryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<SupplierResource> createSupplier(@RequestBody CreateSupplierResource resource) {
+        var createSupplierCommand = CreateSupplierCommandFromResourceAssembler.toCommandFromResource(resource);
+        var supplierId = supplierCommandService.handle(createSupplierCommand);
+        if (supplierId == 0L) {
+            return ResponseEntity.badRequest().build();
+        }
+        var getSupplierByIdQuery = new GetSupplierByIdQuery(supplierId);
+        var supplier = supplierQueryService.handle(getSupplierByIdQuery);
+
+        if (supplier.isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        var supplierResource = SupplierResourceFromEntityAssembler.toResourceFromEntity(supplier.get());
+        return new ResponseEntity<>(supplierResource, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{supplierId}")
+    public ResponseEntity<SupplierResource> getSupplierById(@PathVariable Long supplierId) {
+        var getSupplierByIdQuery = new GetSupplierByIdQuery(supplierId);
+        var supplier = supplierQueryService.handle(getSupplierByIdQuery);
+        if (supplier.isEmpty()) return ResponseEntity.badRequest().build();
+        var supplierResource = SupplierResourceFromEntityAssembler.toResourceFromEntity(supplier.get());
+        return ResponseEntity.ok(supplierResource);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SupplierResource>> getAllSuppliers() {
+        var getAllSuppliersQuery = new GetAllSuppliersQuery();
+        var suppliers = supplierQueryService.handle(getAllSuppliersQuery);
+        var supplierResources = suppliers.stream()
+                .map(SupplierResourceFromEntityAssembler::toResourceFromEntity)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(supplierResources);
+    }
+}
