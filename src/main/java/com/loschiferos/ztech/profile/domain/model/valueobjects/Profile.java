16a96ec6ed45acf05ed666b3739fe10f723307a7
marzzioChicana <u202022228@upc.edu.pe>
2024-06-07 14:07:51 -0500

    update exceptions to Profile

--- src/main/java/com/loschiferos/ztech/profile/domain/model/valueobjects/Profile.java
+++ src/main/java/com/loschiferos/ztech/profile/domain/model/valueobjects/Profile.java
@@ -11,25 +11,4 @@ public record Profile(
     public Profile() {
         this(null, null, null, null, null, null);
     }
-
-    public Profile {
-        if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("Name cannot be null or blank");
-        }
-        if (email == null || email.isBlank()) {
-            throw new IllegalArgumentException("Email cannot be null or blank");
-        }
-        if (password == null || password.isBlank()) {
-            throw new IllegalArgumentException("Password cannot be null or blank");
-        }
-        if (address == null || address.isBlank()) {
-            throw new IllegalArgumentException("Address cannot be null or blank");
-        }
-        if (phone == null) {
-            throw new IllegalArgumentException("Phone cannot be null or blank");
-        }
-        if (photo == null || photo.isBlank()) {
-            throw new IllegalArgumentException("Photo cannot be null or blank");
-        }
-    }
 }
