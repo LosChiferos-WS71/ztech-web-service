c7bd7cff96b5b6dab914b1fada11bb907273bfa5
marzzioChicana <u202022228@upc.edu.pe>
2024-06-16 21:34:27 -0500

    update PlantTypeResourceFromEntityAssembler

--- src/main/java/com/loschiferos/ztech/pot/interfaces/rest/transform/PlantTypeResourceFromEntityAssembler.java
+++ src/main/java/com/loschiferos/ztech/pot/interfaces/rest/transform/PlantTypeResourceFromEntityAssembler.java
@@ -5,6 +5,6 @@ import com.loschiferos.ztech.pot.interfaces.rest.resources.PlantTypeResource;
 
 public class PlantTypeResourceFromEntityAssembler {
     public static PlantTypeResource toResourceFromEntity(PlantType entity) {
-        return new PlantTypeResource(entity.getId(), entity.getScientificName(), entity.getName(), entity.getPhoto(), entity.getDescription());
+        return new PlantTypeResource(entity.getId(), entity.getName(), entity.getScientificName(), entity.getPhoto(), entity.getDescription());
     }
 }
